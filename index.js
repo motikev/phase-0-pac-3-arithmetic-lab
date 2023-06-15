265935e2eff25f9e0cb033039c810ebbc6890110
motikev <kmotties@gmail.com>
2023-06-15 21:02:52 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,84 @@
+const { before } = require("mocha")
+
+function add(){
+
+}
+
+function subtract (){
+
+}
+
+ function multiply(){
+
+ }
+ function divide (){
+
+ }
+
+
+ function increment(n){
+    return n +=1
+ }
+ increment(478)
+
+
+ function decrement(n){
+    return n -=1
+ }
+ decrement(822)
+
+ function add (a,b){
+    return a+b
+ }
+    add(1000, 204)
+
+function subtract(a,b){
+    return a-b
+
+}
+subtract (100-4)
+
+function multiply(a,b){
+return a*b
+}
+    multiply(54252,16)
+
+function divide(a,b){
+    return a/b
+    }
+    divide(5.8641975308641,150)
+
+
+
+function makeInt() {
+      return parseInt(n);
+    }
+
+    makeInt();
+
+var n = "12";
+function makeInt(n){
+    return parseInt(n)
+}
+makeInt()
+
+
+function makeInt(n,){
+    return parseInt(n, 10)
+}
+makeInt("0")
+
+
+function preserveDecimal(n){
+    return parseFloat(n)
+}
+preserveDecimal("2.22")
+
+function preserveDecimal(n){
+    return parseInt(n);
+}
+preserveDecimal("nonsense");
+
+function preserveDecimal(n) {
+    return parseFloat(n);
+  }
